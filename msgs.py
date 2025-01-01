636b51f7481508d01f771c98955f88e198d2b74b
MahdiHabibiXr <themahdihabibi@gmail.com>
2025-01-01 17:03:55 +0330

    first

---
+++ msgs.py
@@ -0,0 +1,8 @@
+start = "Hi, Just send me a voice."
+voice_select = "Got it. Choose the voice"
+voice_btns = [
+    ["ترامپ", "callback", "voice_trump", 0],
+    ["فردوسی پور", "callback", "voice_ferdosipoor", 0],
+    ["ناصر طهماسب", "callback", "voice_tahmasb", 0],
+    ["هایده", "callback", "voice_haideh", 0],
+]
